e824a460fd10f5b9a133823ff6949772de26e45f
Caitlin Sweeney <caitlin.marie.sweeney@gmail.com>
2017-08-21 17:10:19 -0600

    add Animation Extension. Need to add refresh to VC and/or CityVC near navigation.

--- TravelerDemoApp/RefreshCities.swift
+++ TravelerDemoApp/RefreshCities.swift
@@ -11,13 +11,14 @@ import UIKit
 import PullToRefresh
 
 open class PullCities: PullToRefresh {
-    let awesomeRefrehser = PullToRefresh(refresherView: RefreshIconView, animator: RefreshAnimator)
     
-//    public convenience init(at position: Position = .top) {
-//    let refreshView = Bundle(for: type(of: self)).loadNibNamed("RefreshIconView", owner: nil, options: nil)!.first as! RefreshIconView
-//    let animator =  SoupAnimator(refreshView: refreshView)
-//    self.init(refreshView: refreshView, animator: animator, height : refreshView.frame.size.height, position : position)
-//    }
+    public convenience init(at position: Position = .top) {
+    
+        let refreshView = Bundle(for: type(of: self)).loadNibNamed("RefreshIconView", owner: nil, options: nil)!.first as! RefreshIconView
+        let animator =  RefreshIconAnimator(refreshView: refreshView)
+   
+        self.init(refreshView: refreshView, animator: animator, height : refreshView.frame.size.height, position : position)
+    }
 
 }
 
@@ -25,13 +26,57 @@ class RefreshIconView: UIView {
     @IBOutlet weak var refreshImage: UIImageView!
 }
 
-class RefreshAnimator: RefreshViewAnimator {
+class RefreshIconAnimator: NSObject, RefreshViewAnimator {
+    
+   // let timer: Timer?
+    fileprivate let animationDuration = 0.3
+    let animationY = CAKeyframeAnimation(keyPath: "transform.translation.y")
+    
     private let refreshView: RefreshIconView
+   
     init(refreshView: RefreshIconView) {
         self.refreshView = refreshView
     }
     
-    func animateState(state: State) {
-        // animate refreshView according to state
+    public func animate(_ state: State) {
+        switch state {
+            case .initial: initalLayout()
+            case .releasing(let progress): releasingAnimation(progress)
+            case .loading: startLoading()
+            case .finished: refreshView.removeFromSuperview()
+        }
     }
+    
+    // MARK: Helpers
+    
+    func initalLayout() {
+        let centerX = refreshView.frame.size.width / 2
+        //let refreshViewHeight: CGFloat
+
+        // MARK: refreshImage animation
+        
+        refreshView.refreshImage.removeAllAnimations()
+        
+        refreshView.refreshImage.center = CGPoint(x: centerX, y: refreshView.refreshImage.center.y)
+        refreshView.refreshImage.addAnimation(CAKeyframeAnimation.animationWith(
+            AnimationType.TranslationY,
+            values: [-200, 0],
+            keyTimes: [0, 0.5],
+            duration: animationDuration,
+            beginTime:0))
+        refreshView.refreshImage.layer.timeOffset = 0.0
+    }
+    
+    func releasingAnimation(_ progress: CGFloat) {
+        let speed: CGFloat = 1.5
+        let speededProgress: CGFloat = progress * speed > 1 ? 1 : progress * speed
+        
+        refreshView.refreshImage.alpha = progress
+        refreshView.refreshImage.layer.timeOffset = Double(speededProgress) * animationDuration
+    }
+    
+    func startLoading() {
+        refreshView.layer.timeOffset = animationDuration
+    }
+    
 }
